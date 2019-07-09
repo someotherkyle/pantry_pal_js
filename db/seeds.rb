d52026e7766eaa64adcd73435c8990ad9756b857
someotherkyle <kylemclellan@gmail.com>
2019-07-09 13:20:20 -0400

    added seed ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create([
+  {name: "Olive Oil", wet: true},
+  {name: "Vegetable Oil", wet: true},
+  {name: "Balsamic Vinegar", wet: true},
+  {name: "Salt", wet: false},
+  {name: "Pepper", wet: false},
+  {name: "Red Wine Vinegar", wet: true},
+  {name: "Garlic", wet: false},
+  {name: "Black Beans", wet: false},
+  {name: "Tomato Paste", wet: true},
+  {name: "Marinara Sauce", wet: true},
+  {name: "Rice", wet: false},
+  {name: "All-Purpose Flour", wet: false},
+  {name: "Sugar", wet: false},
+  {name: "Spaghetti", wet: false},
+  {name: "Linguine", wet: false},
+  {name: "Rotini", wet: false},
+  {name: "Potatoes", wet: false},
+  {name: "Onion", wet: false},
+  {name: "Eggs", wet: false},
+  {name: "Milk", wet: true},
+  {name: "Yogurt", wet: true},
+  {name: "Mustard", wet: true},
+  {name: "Mayonaise", wet: true},
+  {name: "Ketchup", wet: true},
+  {name: "Milk", wet: true},
+  {name: "Chicken", wet: false},
+  {name: "Ground Beef", wet: false},
+  {name: "Lemon", wet: false},
+  {name: "Apple", wet: false},
+  {name: "Banana", wet: false},
+  {name: "Pickles", wet: false},
+  {name: "Lettuce", wet: false},
+  {name: "Honey Mustard", wet: true},
+  {name: "Jalapeno", wet: false},
+  {name: "Bread", wet: false},
+  {name: "Almond", wet: false}
+])
+
