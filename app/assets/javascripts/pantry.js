8157f6cdff5995517c97c6c468cc0a21b30ca64f
someotherkyle <kylemclellan@gmail.com>
2019-07-11 10:00:29 -0400

    got new pantry form to display on user show page w/o reload

--- app/assets/javascripts/pantry.js
+++ app/assets/javascripts/pantry.js
@@ -13,7 +13,12 @@ function pantryLinkListener(){
 }
 
 function displayPantryForm(){
-  const formHTML =
+  const formHTML = `
+  <input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="Bb2i6MdE8SEKVW3HK3aCDAowhVUMGTV9ye2tSJ23u1smv/7UIKfoE+/6agXVkWUzZR2po6wLaPa67MNLb851jA==">
+  <label for="pantry_location">Location</label>
+  <input type="text" name="pantry[location]" id="pantry_location">
+  <input type="submit" name="commit" value="Create Pantry" data-disable-with="Create Pantry">
+`
   document.getElementById("pantry-form").innerHTML = formHTML;
 }
 
